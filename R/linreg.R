7b0ad40f6a2f7e74931798a9b101701a1326e229
Ruben <RUBEN@MXROBOTS.COM>
2018-09-22 17:03:30 +0200

    Update vignettes content

--- R/linreg.R
+++ R/linreg.R
@@ -14,166 +14,181 @@
 #' @field l_var_beta matrix. Variance of the Regression Coefficients.
 #' @field l_formula formula. The Formula for the Linear Regression.
 #' @field l_p_values numeric. P-Values.
-#' @field l_t_beta matrix. T-Values for Each Coefficient.
+#' @field l_t_beta matrix. T-Values for each Coefficient.
 #' @field l_data_set_name character. The Given Data.
 #'
 #' @return Nothing.
 #' @export
 
 linreg <- setRefClass("linreg",
-    fields = list(
-      l_X = "matrix",
-      l_y = "matrix",
-      l_beta = "matrix",
-      l_y_fitted_values = "matrix",
-      l_e = "matrix",
-      l_n = "numeric",
-      l_p = "numeric",
-      l_df = "numeric",
-      l_sigma_s = "matrix",
-      l_var_beta = "matrix",
-      l_formula = "formula",
-      l_p_values = "numeric",
-      l_t_beta = "matrix",
-      l_data_set_name = "character"),
+                      fields = list(
+                        l_X = "matrix",
+                        l_y = "matrix",
+                        l_beta = "matrix",
+                        l_y_fitted_values = "matrix",
+                        l_e = "matrix",
+                        l_n = "numeric",
+                        l_p = "numeric",
+                        l_df = "numeric",
+                        l_sigma_s = "matrix",
+                        l_var_beta = "matrix",
+                        l_formula = "formula",
+                        l_p_values = "numeric",
+                        l_t_beta = "matrix",
+                        l_data_set_name = "character"),
 
-    # Methods ----------------------------
-    methods = list(
+                      # Methods ----------------------------
+                      methods = list(
 
-      # Constructor ----------------------
-      initialize = function(formula, data) {
+                        # Constructor ----------------------
+                        initialize = function(formula, data) {
+                          "Constructor for creating the object. Arguments are the formlua and the corresponding data frame."
 
-        # Input Validation
-        #if (!is.formula(formula)) stop("Error messgage")
-        #stopifnot(class(formula) == "formula", error = stop("formula is invalid."))
+                          # Input Validation
+                          if (class(formula) != "formula") stop("Argument 'formula' must have the class formula.")
+                          if (class(data) != "data.frame") stop("Argument 'data' must have the class data.frame")
 
-        l_X <<- model.matrix(formula, data)
-        l_y <<- as.matrix(data[all.vars(formula)[1]])
+                          l_X <<- model.matrix(formula, data)
+                          l_y <<- as.matrix(data[all.vars(formula)[1]])
 
-        # Calculate Regressions Coefficients
-        ## Remember he told something about that solve doesn't always works, depends on the eigenvalues.
-        l_beta <<- as.matrix((solve((t(l_X) %*% l_X)) %*% t(l_X) %*% l_y))
+                          # Calculate Regressions Coefficients
+                          ## Remember he told something about that solve doesn't always works, depends on the eigenvalues.
+                          l_beta <<- as.matrix((solve((t(l_X) %*% l_X)) %*% t(l_X) %*% l_y))
 
-        # The Fitted Values
-        l_y_fitted_values <<- l_X %*% l_beta
+                          # The Fitted Values
+                          l_y_fitted_values <<- l_X %*% l_beta
 
-        # The Residuals
-        l_e <<- l_y - l_y_fitted_values
+                          # The Residuals
+                          l_e <<- l_y - l_y_fitted_values
 
-        # Calculate The degrees of freedom
-        # Where n is the number of observations and p is the number of parameters in the model.
-        l_n <<- nrow(l_X)
-        l_p <<- ncol(l_X)
+                          # Calculate The degrees of freedom
+                          # Where n is the number of observations and p is the number of parameters in the model.
+                          l_n <<- nrow(l_X)
+                          l_p <<- ncol(l_X)
 
-        # Degrees of Freedom
-        l_df <<- l_n - l_p
+                          # Degrees of Freedom
+                          l_df <<- l_n - l_p
 
-        # The Residual Variance
-        l_sigma_s <<- (t(l_e) %*% l_e) / l_df
+                          # The Residual Variance
+                          l_sigma_s <<- (t(l_e) %*% l_e) / l_df
 
-        # Variance of Regression Coefficients
-        l_var_beta <<- as.numeric(l_sigma_s) * solve(t(as.matrix(l_X)) %*% as.matrix(l_X))
+                          # Variance of Regression Coefficients
+                          l_var_beta <<- as.numeric(l_sigma_s) * solve(t(as.matrix(l_X)) %*% as.matrix(l_X))
 
-        # Calculate The t-values for each coefficient
-        l_t_beta <<- l_beta / as.numeric(sqrt(var(l_beta))) ## These calculated values are wrong
+                          # Calculate The t-values for each coefficient
+                          l_t_beta <<- l_beta / as.numeric(sqrt(var(l_beta))) ## These calculated values are wrong
 
-        ## Saving
-        l_formula <<- formula
-        l_data_set_name <<- deparse(substitute(data))
-        l_p_values <<- sapply(l_y, pt, q = ncol(l_X), df = l_df)
-      },
+                          ## Saving
+                          l_formula <<- formula
+                          l_data_set_name <<- deparse(substitute(data))
+                          l_p_values <<- sapply(l_y, pt, q = ncol(l_X), df = l_df)
+                        },
 
-      # print function ------------------
-      print = function() {
-        # Formula
-        cat(paste("linreg(formula = ", format(l_formula), ", data = ", l_data_set_name, ")\n\n", sep = ""))
+                        # print function ------------------
+                        print = function() {
+                          "Prints some basic information like the coefficients."
 
-        # Coefficients
-        cat(paste("Coefficients:\n\n"))
+                          # Formula
+                          cat(paste("linreg(formula = ", format(l_formula), ", data = ", l_data_set_name, ")\n\n", sep = ""))
 
-        # Values
-        table = setNames(data.frame(matrix(ncol = length(l_beta), nrow = 0)), rownames(l_beta))
-        for (i in 1:length(l_beta)) {
-          table[1,i] = round(l_beta[i], 3)
-        }
-        cPrint(table)
-      },
+                          # Coefficients
+                          cat(paste("Coefficients:\n\n"))
 
-      # plot function -------------------
-      plot  = function() {
-        #resfit = as.data.frame(cbind(l_e, l_y))
-        #names(resfit) = c("residuals", "fitted")
+                          # Values
+                          table = setNames(data.frame(matrix(ncol = length(l_beta), nrow = 0)), rownames(l_beta))
+                          for (i in 1:length(l_beta)) {
+                            table[1,i] = round(l_beta[i], 3)
+                          }
+                          cPrint(table)
+                        },
+
+                        # plot function -------------------
+                        plot  = function() {
+                          "Plots the Fitted Values one time with the Residuals and one time with standardized Residuals"
+
+                          #resfit = as.data.frame(cbind(l_e, l_y))
+                          #names(resfit) = c("residuals", "fitted")
+
+                          plot1 = ggplot(data.frame(l_e, l_y_fitted_values), aes(y = l_e, x = l_y_fitted_values))+
+                            geom_point()+
+                            xlab(paste("Fitted Values\n", "lm(", format(l_formula), ")", ""))+
+                            ylab("Residuals")+
+                            stat_summary(aes(y = l_e, x = l_y_fitted_values ,group=1),
+                                         fun.y=median, colour="red", geom="line",group=1)
+                          # geom_smooth(aes(y = l_e, x = l_y_fitted_values),
+                          #             formula = y~x,
+                          #             se = FALSE,
+                          #             span = 1,
+                          #             color = "red",
+                          #             method = "auto")
+
+                          cPrint(plot1)
+
+                          stdresfit = as.data.frame(cbind(sqrt(abs((l_e - mean(l_e)))), l_y_fitted_values))
+                          names(stdresfit) = c("stdResiduals", "fitted")
+
+                          plot2 = ggplot(stdresfit, aes(x = fitted, y = stdResiduals))+
+                            geom_point()+
+                            xlab(paste("Fitted Values\n", "lm(", format(l_formula), ")", ""))+
+                            ylab(expression(sqrt("|Standardized residuals|")))+
+                            stat_summary(aes(y = stdResiduals, x = fitted ,group=1),
+                                         fun.y= mean, colour="red", geom="line",group=1)
 
 
-        plot1 = ggplot(data.frame(l_e, l_y_fitted_values), aes(y = l_e, x = l_y_fitted_values))+
-          geom_point()+
-          xlab(paste("Fitted Values\n", "lm(", format(l_formula), ")", ""))+
-          ylab("Residuals")+
-          stat_summary(aes(y = l_e, x = l_y_fitted_values ,group=1),
-                       fun.y=median, colour="red", geom="line",group=1)
-          # geom_smooth(aes(y = l_e, x = l_y_fitted_values),
-          #             formula = y~x,
-          #             se = FALSE,
-          #             span = 1,
-          #             color = "red",
-          #             method = "auto")
 
-        cPrint(plot1)
+                          # geom_smooth(aes(y = stdResiduals, x = fitted),
+                          #             formula = y~x,
+                          #             se = FALSE,
+                          #             span = 1,
+                          #             color = "red",
+                          #             method = "auto")
+                          cPrint(plot2)
+                        },
 
-        stdresfit = as.data.frame(cbind(sqrt(abs((l_e - mean(l_e)))), l_y_fitted_values))
-        names(stdresfit) = c("stdResiduals", "fitted")
+                        # resid function -------------------
+                        resid     = function() {
+                          "Returns the Residuals"
+                          return(l_e)
+                        },
 
-        plot2 = ggplot(stdresfit, aes(x = fitted, y = stdResiduals))+
-          geom_point()+
-          xlab(paste("Fitted Values\n", "lm(", format(l_formula), ")", ""))+
-          ylab(expression(sqrt("|Standardized residuals|")))+
-          stat_summary(aes(y = stdResiduals, x = fitted ,group=1),
-                       fun.y= mean, colour="red", geom="line",group=1)
+                        # pred function --------------------
+                        pred      = function() {
+                          "Returns the Fitted Values"
+                          return(l_y_fitted_values)
+                        },
 
+                        # ceof function --------------------
+                        coef      = function() {
+                          "Returns the Coefficients"
+                          return(l_beta)
+                        },
 
+                        # summary function -----------------
+                        summary   = function() {
+                          "Prints a summary of the Linear Regression"
 
-        # geom_smooth(aes(y = stdResiduals, x = fitted),
-          #             formula = y~x,
-          #             se = FALSE,
-          #             span = 1,
-          #             color = "red",
-          #             method = "auto")
-        cPrint(plot2)
-      },
+                          cat("\nCall:\n")
+                          cat(paste("linreg(formula = ", (format(l_formula)), ", data = ", l_data_set_name, ")\n\n", sep = ""))
 
-      # resid function -------------------
-      resid     = function() { return(l_e) },
+                          # Coefficients
+                          cat("Coefficients:\n\n")
 
-      # pred function --------------------
-      pred      = function() { return(l_y_fitted_values) },
+                          # Values
+                          table = data.frame(matrix(ncol = 5, nrow = 0))
+                          for (i in 1:length(l_beta)) {
+                            # Beta (coefficients), std error, t values, p values
+                            local_t_value = l_beta[i]/sqrt(l_var_beta[i, i])
+                            local_p_value = 2 * pt(abs(local_t_value), l_df, lower.tail = FALSE)
+                            newRow = data.frame(round(l_beta[i], 2), round(sqrt(l_var_beta[i, i]), 2), round(local_t_value, 2), formatC(local_p_value, format = "e", digits = 2), calculateMagicRainbowStars(local_p_value))
+                            rownames(newRow)[1] = rownames(l_var_beta)[i]
+                            table = rbind(table, newRow)
+                          }
 
-      # ceof function --------------------
-      coef      = function() { return(l_beta) },
-
-      # summary function -----------------
-      summary   = function() {
-
-        cat("\nCall:\n")
-        cat(paste("linreg(formula = ", (format(l_formula)), ", data = ", l_data_set_name, ")\n\n", sep = ""))
-
-        # Coefficients
-        cat("Coefficients:\n\n")
-
-        # Values
-        table = data.frame(matrix(ncol = 4, nrow = 0))
-        for (i in 1:length(l_beta)) {
-          # Beta (coefficients), std error, t values, p values
-          local_t_value = l_beta[i]/sqrt(l_var_beta[i, i])
-          newRow = data.frame(round(l_beta[i], 2), round(sqrt(l_var_beta[i, i]), 2), round(local_t_value, 2), "***")
-          rownames(newRow)[1] = rownames(l_var_beta)[i]
-          table = rbind(table, newRow)
-        }
-
-        colnames(table) = c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
-        cPrint(table, TRUE)
-        cat(paste("\nResidual standard error:", sqrt(l_sigma_s), "on", l_df, "degrees of freedom"))
-      }
-    )
+                          colnames(table) = c("Estimate", "Std. Error", "t value", "Pr(>|t|)", "")
+                          cPrint(table, TRUE)
+                          cat(paste("\nResidual standard error:", sqrt(l_sigma_s), "on", l_df, "degrees of freedom"))
+                        }
+                      )
 )
 
 #' cPrint (custom print)
@@ -184,7 +199,6 @@ linreg <- setRefClass("linreg",
 #' @param stripoff If set to TRUE and \code{x} is a data.frame, the column names will be stripped off.
 #'
 #' @return Nothing.
-#' @export
 cPrint = function(x, stripoff = FALSE) {
   if (is.data.frame(x)) {
     print(x, row.names = stripoff)
@@ -194,7 +208,23 @@ cPrint = function(x, stripoff = FALSE) {
   }
 }
 
+#' calculateMagicRainbowStars
+#'
+#' Returns different notations depending which value the p_value holds.
+#'
+#' @param p_value Obviously the p_value.
+#'
+#' @return Returns: Signif. codes:  0 "***" 0.001 "**" 0.01 "*" 0.05 "." 0.1 " " 1
+#'
+calculateMagicRainbowStars = function(p_value) {
+  if (p_value > 0.1) return(" ")
+  if (p_value > 0.05) return(".")
+  if (p_value > 0.01) return("*")
+  if (p_value > 0.001) return("**")
+  return("***")
+}
+
 linreg_mod = linreg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
-linreg_mod$print()
-linreg_mod$summary()
-#linreg_mod$plot()
+#linreg_mod$print()
+#linreg_mod$summary()
+linreg_mod$plot()
